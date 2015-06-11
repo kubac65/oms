ab3dd8db6540c04f245d13fc6986860c70eae100
Jakub Potocki <kubac65@gmail.com>
2015-06-11 23:39:08 +0100

    Customer Remove and Add modals partially implemented

--- WebApp/static/js/oms-customers.js
+++ WebApp/static/js/oms-customers.js
@@ -1,29 +1,142 @@
 (function(){
-	var app = angular.module('oms-customers', []);
+	var app = angular.module('oms-customers', ['ui.bootstrap']);
 
-	app.directive('customersList', function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'customers-list.html',
-			controller: function(){
-				this.customers = customers;
+	app.controller('CustomersController', function($modal){
+		this.customers = customers;
 
-				this.remove = function(customer){
-					var index = this.customers.indexOf(customer);
-					this.customers.splice(index, 1);
-				};
-				this.add = function(customer){
-					this.customers.push(customer);
-				}
-			},
-			controllerAs: 'customersCtrl'
-		}
+		this.add = function(){
+			var customers = this.customers;
+
+			$modal.open({
+				templateUrl: 'addUserModal.html',
+				controller: function($modalInstance){
+					this.customer = {};
+
+					this.add = function(){
+						customers.push(this.customer);
+						this.customer = {};
+						$modalInstance.dismiss('Customer Saved');
+					};
+
+					this.cancel = function(){
+						$modalInstance.dismiss('Cancel');
+					};
+				},
+				controllerAs: 'addUserCtrl'
+			});
+		};
+
+		this.remove = function(customer){
+			var customers = this.customers;
+
+			$modal.open({
+				templateUrl: 'removeUserModal.html',
+				controller: function($modalInstance){
+					this.customer = customer
+
+					this.yes = function(){
+						var index = customers.indexOf(customer);
+						customers.splice(index, 1);
+						$modalInstance.dismiss('Customer Removed');
+					};
+
+					this.no = function(){
+						$modalInstance.dismiss('Cancel');
+					};
+				},
+				controllerAs: 'remUserCtrl'
+			});
+		};
+
+		this.search = function(text){
+
+		};
 	});
 
 	var customers = [
 		{
 			"id": 1,
 			"name": "Jakub Potocki",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},{
+			"id": 2,
+			"name": "Agata",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},{
+			"id": 3,
+			"name": "Guziec",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},
+		{
+			"id": 5,
+			"name": "Krysia",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},{
+			"id": 2,
+			"name": "Agata",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},{
+			"id": 3,
+			"name": "Guziec",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},
+		{
+			"id": 5,
+			"name": "Krysia",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},{
+			"id": 2,
+			"name": "Agata",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},{
+			"id": 3,
+			"name": "Guziec",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},
+		{
+			"id": 5,
+			"name": "Krysia",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},{
+			"id": 2,
+			"name": "Agata",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},{
+			"id": 3,
+			"name": "Guziec",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},
+		{
+			"id": 5,
+			"name": "Krysia",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},{
+			"id": 2,
+			"name": "Agata",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},{
+			"id": 3,
+			"name": "Guziec",
+			"address": "24 Main Street Cork",
+			"contact": "0862548795"
+		},
+		{
+			"id": 5,
+			"name": "Krysia",
 			"address": "24 Main Street Cork",
 			"contact": "0862548795"
 		},{
