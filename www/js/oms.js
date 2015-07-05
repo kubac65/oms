30e3e5cab136a8f0580449c83cafeee871f3f6d2
Jakub Potocki <kubac65@gmail.com>
2015-07-05 22:28:05 +0100

    Angular router initial setup

--- www/js/oms.js
+++ www/js/oms.js
@@ -1,14 +1,17 @@
 (function(){
 	var app = angular.module('oms', ['ui.router', 'oms.customers']);
 
-	app.config(function($stateProvider, $urlRouterProvider) {
-
-		$urlRouterProvider.otherwise('/wrong-url');
+	app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
 
 		$stateProvider
+			.state('jobs', {
+				url: '/jobs',
+				templateUrl: 'jobs/jobs.template.html'
+			})
 			.state('customers', {
 				url: '/customers',
-				templateUrl: 'customers'
+				templateUrl: 'customers/customers.template.html'
 			});
-	});
+	}]);
 })();
