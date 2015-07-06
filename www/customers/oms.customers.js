701d640219f8a47de4854a85ffa47f140026cb50
Jakub Potocki <kubac65@gmail.com>
2015-07-06 22:44:04 +0100

    customers service added, adding and removing customers implemented (frontend)

--- www/customers/oms.customers.js
+++ www/customers/oms.customers.js
@@ -1,30 +1,34 @@
 (function(){
-	var app = angular.module('oms.customers', ['oms.customers.directives', 'ui.bootstrap']);
+	var app = angular.module('oms.customers', ['oms.customers.directives', 'oms.customers.service', 'ui.bootstrap']);
 
 	app.controller('CustomersListController', customerListCtrl);
-	customerListCtrl.$inject = ['$scope', '$modal'];
-	function customerListCtrl($scope, $modal){
-		$scope.customers = customers;
 
-		$scope.addNew = function(){
+	customerListCtrl.$inject = ['$scope', '$modal', 'customersService'];
+	function customerListCtrl($scope, $modal, customersService){
+		$scope.customers = customersService.customers;
+
+		$scope.add = function(){
 			$modal.open({
 				templateUrl: 'customers/customer-addnew-modal.template.html',
 				size: 'lg',
 				controller: addNewCustomerCtrl
 			});
 
-			addNewCustomerCtrl.$inject = ['$scope','$modalInstance'];
-			function addNewCustomerCtrl($scope, $modalInstance){
+			addNewCustomerCtrl.$inject = ['$scope', '$modalInstance', 'customersService'];
+			function addNewCustomerCtrl($scope, $modalInstance, customersService){
 				$scope.customer = {};
-				$scope.cancel = function(){
+
+				$scope.add = function(){
+					customersService.add($scope.customer);
 					$modalInstance.dismiss();
 				};
-				$scope.add = function(){
-					// Do some black magic with $scope.customer
+
+				$scope.cancel = function(){
 					$modalInstance.dismiss();
 				};
 			}
 		};
+
 		$scope.remove = function(customer){
 			$modal.open({
 					templateUrl: 'customers/customer-remove-modal.template.html',
@@ -32,80 +36,19 @@
 					controller: removeCustomerCtrl
 			});
 
-			removeCustomerCtrl.$inject = ['$scope', '$modalInstance'];
-			function removeCustomerCtrl($scope, $modalInstance){
+			removeCustomerCtrl.$inject = ['$scope', '$modalInstance', 'customersService'];
+			function removeCustomerCtrl($scope, $modalInstance, customersService){
 				$scope.customer = customer;
+
+				$scope.yes = function(){
+					customersService.remove($scope.customer);
+					$modalInstance.dismiss();
+				};
+
+				$scope.no = function(){
+					$modalInstance.dismiss();
+				};
 			};
 		};
 	}
-
-	/*app.controller('AddCustomerController', addCustomerCtrl);
-	addCustomerCtrl.$inject = ['$scope', '$modalInstance'];
-	function addCustomerCtrl($scope,$modalInstance){
-		$scope.customer = {};
-
-		$scope.add = function(){
-
-		};
-		$scope.cancel = function(){
-
-		};
-	}*/
-
-	var customers = [
-		{
-			"id": 1,
-			"name": "Jakub i",
-			"address": "24 N Main Street, Cork",
-			"phone": "0862548795",
-			"email": "test@test.com",
-			"jobOrders": [
-				{
-					"id" : 1,
-					"orderDate": "10-08-25",
-					"dueDate": "15-35-25",
-					"order-ref": "Tomasz",
-					"notes": "dupa dupa dupa",
-					"total": 1234.12,
-					"items": [
-						{
-							"description": "plexa 10x10",
-							"quantity": 2,
-							"unitPrice": 10.99,
-							"subtotal": 21.89
-						},
-						{
-							"description": "poster",
-							"quantity": 20,
-							"unitPrice": 1.50,
-							"subtotal": 30
-						},
-
-					]
-				},
-				{
-					"id" : 2,
-					"orderDate": "10-08-25",
-					"dueDate": "15-35-25",
-					"order-ref": "Tomasz tt",
-					"notes": "dupa dupa dupa",
-					"total": 12343123.12,
-					"items": [
-						{
-							"description": "plexa 10x10",
-							"quantity": 2,
-							"unitPrice": 10.99,
-							"subtotal": 21.89
-						},
-						{
-							"description": "poster",
-							"quantity": 20,
-							"unitPrice": 1.50,
-							"subtotal": 30
-						}
-					]
-				}
-			]
-		}
-	];
 })();
---
+++ www/customers/oms.customers.service.js
@@ -0,0 +1,74 @@
+(function(){
+	var app = angular.module('oms.customers.service', []);
+  app.service('customersService', function(){
+    return {
+      customers: _customers,
+      add: function(customer){
+        // Make request to backend
+        this.customers.push(customer);
+      },
+      remove: function(customer){
+        // Make request to backend
+        var index = this.customers.indexOf(customer);
+				this.customers.splice(index, 1);
+      }
+    }
+  });
+
+  var _customers = [
+		{
+			"id": 1,
+			"name": "Jakub i",
+			"address": "24 N Main Street, Cork",
+			"phone": "0862548795",
+			"email": "test@test.com",
+			"jobOrders": [
+				{
+					"id" : 1,
+					"orderDate": "10-08-25",
+					"dueDate": "15-35-25",
+					"order-ref": "Tomasz",
+					"notes": "dupa dupa dupa",
+					"total": 1234.12,
+					"items": [
+						{
+							"description": "plexa 10x10",
+							"quantity": 2,
+							"unitPrice": 10.99,
+							"subtotal": 21.89
+						},
+						{
+							"description": "poster",
+							"quantity": 20,
+							"unitPrice": 1.50,
+							"subtotal": 30
+						},
+
+					]
+				},
+				{
+					"id" : 2,
+					"orderDate": "10-08-25",
+					"dueDate": "15-35-25",
+					"order-ref": "Tomasz tt",
+					"notes": "dupa dupa dupa",
+					"total": 12343123.12,
+					"items": [
+						{
+							"description": "plexa 10x10",
+							"quantity": 2,
+							"unitPrice": 10.99,
+							"subtotal": 21.89
+						},
+						{
+							"description": "poster",
+							"quantity": 20,
+							"unitPrice": 1.50,
+							"subtotal": 30
+						}
+					]
+				}
+			]
+		}
+	];
+})();
