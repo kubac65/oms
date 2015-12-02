13888bdbaebe0af6491f81ac96d3ea90c86c2d9d
Jakub Potocki <kubac65@gmail.com>
2015-12-02 22:55:28 +0000

    Login redirecting after success

--- www/login/ctrl.js
+++ www/login/ctrl.js
@@ -4,16 +4,19 @@
   angular.module('oms.login')
     .controller('LoginController', loginCtrl);
 
-  loginCtrl.$inject = ['$scope', 'AuthService'];
+  loginCtrl.$inject = ['$scope', '$state', 'AuthService'];
 
-  function loginCtrl($scope, AuthService) {
+  function loginCtrl($scope, $state, AuthService) {
     $scope.user = {
       username: '',
       password: ''
     };
 
     $scope.login = function(user) {
-      AuthService.login(user);
+      AuthService.login(user)
+        .then(function(res) {
+          $state.transitionTo("customers");
+        }, function() {});
     };
   };
 })();
--- www/login/service.js
+++ www/login/service.js
@@ -4,26 +4,45 @@
   angular.module('oms.login')
     .service('AuthService', authService);
 
-  authService.$inject = [];
+  authService.$inject = ['$q'];
 
-  function authService() {
-    var authenticated = false;
+  function authService($q) {
+    var auth = {
+      user: {},
+      authenticated: false
+    };
 
     this.login = function(user) {
+      var defer = $q.defer();
+
       dpd.users.login(user)
         .then(function success(result){
-          authenticated = true;
-        }, function err(err){
-          throw err;
+          auth.authenticated = true;
+          defer.resolve(result);
+        }, function error(err){
+          defer.reject(err);
         });
+
+      return defer.promise;
     };
 
     this.logout = function() {
-      dpd.users.get('logout');
+      var defer = $q.defer();
+
+      dpd.users.logout()
+        .then(function success() {
+          auth.authenticated = false;
+          auth.user = {};
+          defer.resolve();
+        }, function error(err) {
+          defer.reject(err);
+        });
+
+      return defer.promise;
     };
 
     this.isAuthenticated = function() {
-      return authenticated;
+      return auth.authenticated;
     };
   };
 })();
